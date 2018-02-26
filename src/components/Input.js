c2d3b514a00994c9bd8ab03a986fcef438cb2563
Safisyx <safisyx@gmail.com>
2018-02-26 01:15:54 +0100

    Create Input component

---
+++ src/components/Input.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import {guessLetter} from '../actions/action'
+import './Input.css'
+
+export class Input extends PureComponent {
+  static propTypes = {
+    guessLetter: PropTypes.func.isRequired
+  }
+
+  handleChange = (e) => {
+    let x = e.target.value.toLowerCase()
+    //console.log(x)
+    this.props.guessLetter(x)
+    e.target.value=''
+  }
+
+  render() {
+    return (
+      <div className='Input'>
+        Guess a letter: <input
+          type ='text'
+          id = 'guess'
+          onChange={this.handleChange}
+        />
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = ({guessLetter})
+
+export default connect(null, mapDispatchToProps)(Input)
