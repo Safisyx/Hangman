089b7cd40c3c80ea4ec572dc7b0d8fd2ad1045a4
Safisyx <safisyx@gmail.com>
2018-04-26 21:37:37 +0200

    Add more actions

---
+++ src/actions/giveUp.js
@@ -0,0 +1,9 @@
+import {GIVE_UP} from './types'
+//import {words} from '../data/data'
+
+export const giveUp = () => {
+  //console.log(text)
+  return {
+    type: GIVE_UP
+  }
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const GUESS = 'GUESS'
+export const NEW_GAME = 'NEW_GAME'
+export const GIVE_UP = 'GIVE_UP'
